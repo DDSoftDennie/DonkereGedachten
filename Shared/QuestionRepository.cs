ec327d5372ec184c60099c78f33149d62a74cbbc
Dennie Declercq <dennie@ddsoft.be>
2020-12-15 23:01:51 +0100

    BugFix

--- Shared/QuestionRepository.cs
+++ Shared/QuestionRepository.cs
@@ -6,26 +6,27 @@ namespace BlazorApp.Shared
 {
     public class QuestionRepository
     {
-        private string _path;
+        private readonly string _path = "/Cards/";
         private int _index;
         private List<Question> AllQuestions { get; set; }
 
 
        public QuestionRepository()
         {
-            _path = "/Cards/";
             AllQuestions = new List<Question>();
-
-            for (int i = 1; i < 39; i++)
+       
+            for (int i = 1; i <=38; i++)
             {
                 Question q = new Question
                 {
                     Number = i,
                     Path = _path
                 };
+          
                 AddQuestion(q);
+    
             }
-            _index = 0;
+      
 
         }
 
@@ -36,31 +37,24 @@ namespace BlazorApp.Shared
 
         public Question NextQuestion()
         {
-            if (_index <= 39)
+            if (_index < 37)
             {
                 _index += 1;
             }
             else
+            {
                 _index = 0;
+            }
 
+        
             Question q = AllQuestions[_index];
+ 
             return q;
         }
 
-        public Question ShowQuestion(bool random = false)
+        public Question ShowQuestion()
         {
- 
-            if(random == false)
-            {
                  return  AllQuestions[_index];            
-            }
-            else
-            {
-                Random rnd = new Random();
-                int _question = rnd.Next(0, 38);
-                return AllQuestions[_question];
-            }
-        
         }
 
     }
